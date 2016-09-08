93b92bdeee2b8db9bcc3039a1b62da70933f92cf
sharath <sharath@ajency.in>
2016-09-08 13:10:13 +0530

    AddedLocalization

--- config/app.php
+++ config/app.php
@@ -66,7 +66,8 @@ return [
     */
 
     'locale' => 'en',
-
+    'locales' => ['en' => 'English', 'fr' => 'Français', 'sv' => 'Swedish'],
+    
     /*
     |--------------------------------------------------------------------------
     | Application Fallback Locale
@@ -194,7 +195,7 @@ return [
         'Password' => Illuminate\Support\Facades\Password::class,
         'Queue' => Illuminate\Support\Facades\Queue::class,
         'Redirect' => Illuminate\Support\Facades\Redirect::class,
-        'Redis' => Illuminate\Support\Facades\Redis::class,
+        /*'LRedis' => Illuminate\Support\Facades\Redis::class,*/'Redis' => Illuminate\Support\Facades\Redis::class,
         'Request' => Illuminate\Support\Facades\Request::class,
         'Response' => Illuminate\Support\Facades\Response::class,
         'Route' => Illuminate\Support\Facades\Route::class,
