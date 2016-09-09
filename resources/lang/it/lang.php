779a6ff35be1daa94e7bc3a2d851b67e1a4b924d
sharath <sharath@ajency.in>
2016-09-09 11:17:54 +0530

    new Language (Italian)

---
+++ resources/lang/it/lang.php
@@ -0,0 +1,62 @@
+<?php
+	// Italian - Italiano
+	return [
+	  'welcome' => 'Benvenuto',
+	  'active' => 'Attivo',
+	  'idle' => 'Inattivo',
+	  'offline' => 'Disconnesso',
+	  'hrs' => 'Ore',
+	  'mins' => 'Minuti',
+	  '2dy_wrk_hrs_cntrbtd' => 'Oggi le ore di lavoro hanno contribuito', // Di oggi -> today
+	  'member_set' => 'Impostare manualmente dal membro',
+	  'hrs_cntrbtd_2dy' => 'Ore contribuito oggi',
+	  'msg' => 'Esempio laravel Internazionalizzazione.',
+	  'view_emp_det' => 'Dettagli dipendenti',
+	  'edit_profile' => 'Modifica Profilo',
+	  'logout' => 'Disconnettersi',
+	  'home' => 'Casa',
+	  'login' => 'Accesso',
+	  'register' => 'registro',
+	  'contact_us' => 'Contattaci',
+	  'now' => 'Adesso',
+	  'today' => 'Oggi',
+	  'by' => 'Di',
+	  'and' => 'e',
+	  'site_lang' => 'La lingua del sito',
+	  'ur_name' => 'Il tuo nome',
+	  'ur_off_email_id' => 'Il tuo ID ufficiale e-mail',
+	  'ur_role_permission' => 'Il tuo ruolo / permessi',
+	  'name' => 'Nome',
+	  'delete' => 'Cancellare',
+	  'domain' => 'Dominio',
+	  'this_week_workout' => 'Allenamento di questa settimana',
+
+
+	  //main page
+	  'hero_title' => 'Tempo traccia. Record lavoro',
+	  'hero_tag' => 'Una soluzione completa di produttività per monitorare il vostro lavoro, i dipendenti lavorano e gestire progetti senza alcun problema. E la cosa migliore? <strong>E\'totalmente gratuito!</strong>',
+	  'get_the' => 'Ottenere il',
+	  'why' => 'Perché',
+	  'main_hero_tag' => 'Tracce il vostro tempo e il lavoro in modo da poter concentrare con facilità',
+	  'the_features' => 'Le caratteristiche',
+	  'features' => '<li>L\'ora esatta di monitoraggio con login e logout timestamp</li><li>Schede attività e le relazioni sono dettagliate e generato automaticamente</li><li>Livello di attività inseguitore</li><li>Sincronizzazione senza soluzione di continuità attraverso il sito web , Chrome app e l\'estensione</li>',
+	  'for_employers' => 'Per i datori di lavoro',
+	  'employers' => '<li>Gestisci la tua organizzazione</li><li>Mantenere una traccia di tutti i progetti attivi</li><li>Aiutare i dipendenti a gestire il loro momento migliore</li><li>Report dettagliati con schede accurate generati quotidianamente</li>',// schede -> timesheets / fogli di presenza -> time sheets
+	  'for_employees' => 'Per i dipendenti',
+	  'employees' => '<li>Gestire tutti i vostri progetti</li><li>Mantenere una traccia di tutte le attività che hai fatto e ancora da fare</li><li>Nessuna distrazione, aumentare la produttività</li><li>Assicurati di raggiungere i vostri obiettivi giornalieri e settimanali</li>',
+	  'main_get_title' => 'Ottieni il tuo Dilbert ora',
+	  'main_get_sub_title' => 'È sufficiente accedere con il tuo account Google e lasciare il caos n trambusto a noi!',
+	  'main_get_sub_btn' => 'Iscriviti Dilbert ora',
+	  
+	  // login & sign-up
+	  'account_exist' => 'Avere un account esistente?',
+	  'sign_in' => 'Registrati',
+	  'sign_up' => 'Registrazione',
+	  'n_cont_whr_u_left_off' => 'e continuare da dove si era interrotto',
+	  'with_google' => 'con Google',
+	  '_change_ur_work_way' => ', cambiare il vostro modo di lavorare !',
+	  'already_hv_acc_in_dil' => 'Già si dispone di un account su Dilbert?',
+	  'not_conn_2_dilbert' => 'Non ancora collegato a Dilbert?',// Not yet connected to dilbert
+	  'conn_using_google_acc' => 'Basta collegare il tuo account Google',
+	];
+?>
