2e234524aba878eb406787ab8b2ec4caf04a1f11
sharath <sharath@ajency.in>
2016-09-12 13:25:06 +0530

    Individual user gets to choose Timezone & Language

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,11 +18,13 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->string('password');
             $table->string('avatar');
+            $table->string('lang');// language, user wants the app & site to be displayed as
+            $table->string('timeZone'); // user choses the time zone which is defined in company list -> also determines the region where user works
             $table->date('acd');//account creation date
             $table->unsignedInteger('org_id')->nullable();
             $table->string('socket_id')->nullable();
             $table->string('role')->nullable();
-            $table->string('api_token', 60) ->nullable();
+            $table->string('api_token', 60) ->nullable(); // for authenticating request from chrome app
             $table->rememberToken();
             $table->timestamps();
         });
