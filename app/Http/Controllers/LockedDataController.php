03b29bb79e2ec0f7819b10dda7e4b31be72d8e9a
Unknown <sharath@ajency.in>
2016-11-11 19:15:50 +0530

    Minor Modifications for Locked_datas data entry

--- app/Http/Controllers/LockedDataController.php
+++ app/Http/Controllers/LockedDataController.php
@@ -49,28 +49,50 @@ class LockedDataController extends Controller
 
 			return  response()->json(['status' => 'Success']);
 		} else if(date('l', strtotime($lastDate->work_date)) != "Saturday" || date_diff(date_create($lastDate->work_date), date_create(date("Y-m-d")))->format("%R%a") != "+1") {
-			/*if previous date is not saturday & today's & last summary date difference is not +1*/
-			$user_ids = Log::select('user_id')->groupBy('user_id')->get();//Only Distinct user id's
+			/*if (previous date is not saturday) or (today's & last summary date difference is not +1)*/
 			
-			foreach($user_ids as $user) {
-				$output->writeln($lastDate->work_date);
-				$log_first = Log::where(['user_id'=> $user->user_id,'work_date' => strftime("%Y-%m-%d", strtotime("$lastDate->work_date +1 day"))])->first();
-				$log_last = Log::where(['user_id'=> $user->user_id,'work_date' => strftime("%Y-%m-%d", strtotime("$lastDate->work_date +1 day"))])->orderBy('cos', 'desc')->orderBy('id', 'desc')->first();
+			$user_ids = Log::select('user_id')->groupBy('user_id')->get();//Only Distinct user id's
 
-				$summary = new Locked_Data;
-				$summary->user_id = $user->user_id;
-				$summary->work_date = strftime("%Y-%m-%d", strtotime("$lastDate->work_date +1 day"));
-				$summary->start_time = $log_first->cos;
-				$summary->end_time = $log_last->cos;
-				$summary->total_time = $this->getTimeDifference($log_first->cos, $log_last->cos);
-				$summary->save();
+			foreach($user_ids as $user) {
+				
+				/* Check if last date in the Locked_datas table is not same as last date in logs table */
+				if (date_diff(date_create($lastDate->work_date), date_create(date("Y-m-d")))->format("%R%a") == "+0") { /* if same then take current date */
+					$log_first = Log::where(['user_id'=> $user->user_id,'work_date' => strftime("%Y-%m-%d", strtotime($lastDate->work_date))])->first();
+					$log_last = Log::where(['user_id'=> $user->user_id,'work_date' => strftime("%Y-%m-%d", strtotime($lastDate->work_date))])->orderBy('cos', 'desc')->orderBy('id', 'desc')->first();
+					$work_date = strftime("%Y-%m-%d", strtotime($lastDate->work_date));
+				} else { /* If not same the take the next date*/
+					$log_first = Log::where(['user_id'=> $user->user_id,'work_date' => strftime("%Y-%m-%d", strtotime("$lastDate->work_date +1 day"))])->first();
+					$log_last = Log::where(['user_id'=> $user->user_id,'work_date' => strftime("%Y-%m-%d", strtotime("$lastDate->work_date +1 day"))])->orderBy('cos', 'desc')->orderBy('id', 'desc')->first();
+					$work_date = strftime("%Y-%m-%d", strtotime("$lastDate->work_date +1 day"));
+				}
+
+				$cross_check = Locked_Data::where(['user_id' => $user->user_id, 'work_date' => $work_date])->count();
+				
+				if($cross_check <= 0) { /* Check if data exist of the user w.r.t that/current date, if no, then add the new data */
+					$summary = new Locked_Data;
+					$summary->user_id = $user->user_id;
+					$summary->work_date = $work_date;//strftime("%Y-%m-%d", strtotime("$lastDate->work_date +1 day"));
+					$summary->start_time = $log_first->cos;
+					$summary->end_time = $log_last->cos;
+					$summary->total_time = $this->getTimeDifference($log_first->cos, $log_last->cos);
+					$summary->save();
+				} else {/* If data exist then update the data */
+					if (date_diff(date_create($lastDate->work_date), date_create(date("Y-m-d")))->format("%R%a") == "+0") { /* If difference between dates is 0, then update*/
+						$summary = Locked_Data::where(['user_id' => $user->user_id, 'work_date' => $work_date])->first();
+						$summary->user_id = $user->user_id;
+						$summary->work_date = $work_date;//strftime("%Y-%m-%d", strtotime("$lastDate->work_date +1 day"));
+						$summary->start_time = $log_first->cos;
+						$summary->end_time = $log_last->cos;
+						$summary->total_time = $this->getTimeDifference($log_first->cos, $log_last->cos);
+						$summary->update();
+					}
+				}
 			}
 
 			return  response()->json(['status' => 'Success']);
 		} else { // Last date i.e. Yesterday was Saturday
 			return  response()->json(['status' => 'Exist']);
 		}
-
 		return  response()->json(['status' => 'Error']);
 	}
 
@@ -129,7 +151,7 @@ class LockedDataController extends Controller
 		    	return response()->json(['status' => 'Error', 'msg' => 'Permission Denied']);
 		    }
 	    } else {
-			$output->writeln("In else");	    	
+			//$output->writeln("In else");	    	
 	    	return response()->json(['status' => 'Error', 'msg' => 'Required parameters not satisfied']);
 	    }
     }
@@ -157,15 +179,14 @@ class LockedDataController extends Controller
     	}
 
     	if($hr > 9)
-    		$total = (string)$hr + ":";
+    		$total = (string)$hr . ":";
     	else
-    		$total = "0" + (string)$hr + ":";
+    		$total = "0" . (string)$hr . ":";
 
     	if($min > 9)
-    		$total = $total + (string)$min;
+    		$total = $total . (string)$min;
     	else
-    		$total = $total + "0" + (string)$min;
-
+    		$total = $total . "0" . (string)$min;
 
     	return $total;
     }
