36a34a9b2347ee816523bc24aa18774f25927023
mohsinAjency <mohsin@ajency.in>
2016-11-16 15:49:16 +0530

    restructured

--- src/app/classes/user.ts
+++ src/app/classes/user.ts
@@ -1,10 +1,7 @@
 export class User {
       id: number;
-      raceId: number;
-      date: Date;
-      currentLap: number;
-      totalLaps: number;
-      trackLength: number;
-      racers: string[];
-
+      name: string;
+      email: string;
+      avatar: string;
+      token: string;
 }
--- src/service-worker.js
+++ src/service-worker.js
@@ -16,25 +16,24 @@ const messaging = firebase.messaging()
 messaging.requestPermission().then(function(){
 
       console.log("NEW PERMISSION");
-messaging.getToken()
-.then(function(currentToken) {
-    if (currentToken) {
-      sendTokenToServer(currentToken);
-      updateUIForPushEnabled(currentToken);
-    } else {
-      // Show permission request.
-      console.log('No Instance ID token available. Request permission to generate one.');
-      // Show permission UI.
-      updateUIForPushPermissionRequired();
-      setTokenSentToServer(false);
-    }
-  })
+// messaging.getToken()
+// .then(function(currentToken) {
+//     if (currentToken) {
+//       sendTokenToServer(currentToken);
+//       updateUIForPushEnabled(currentToken);
+//     } else {
+//       // Show permission request.
+//       console.log('No Instance ID token available. Request permission to generate one.');
+//       // Show permission UI.
+//       updateUIForPushPermissionRequired();
+//       setTokenSentToServer(false);
+//     }
+   })
   .catch(function(err) {
     console.log('An error occurred while retrieving token. ', err);
     showToken('Error retrieving Instance ID token. ', err);
     setTokenSentToServer(false);
   });
-})
 
 
 // Files required to make this app work offline
