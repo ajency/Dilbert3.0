5fbf7002dd6bb895f8b20aaa7ccabfb079a1e7f1
Unknown <sharath@ajency.in>
2016-12-30 15:29:46 +0530

    included ES6 shims for browsers that do not support ES6 code

--- resources/views/angular/index.blade.php
+++ resources/views/angular/index.blade.php
@@ -87,5 +87,9 @@
   <script type="text/javascript" src="{{ url('/views/inline.js') }}"></script>
   <script type="text/javascript" src="{{ url('/views/styles.bundle.js') }}"></script>
   <script type="text/javascript" src="{{ url('/views/scripts.bundle.js') }}"></script>
-  <script type="text/javascript" src="{{ url('/views/main.bundle.js') }}"></script> 
+  <script type="text/javascript" src="{{ url('/views/main.bundle.js') }}"></script>
+  <!-- IE required polyfills, in this exact order -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/es6-shim/0.33.3/es6-shim.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/systemjs/0.19.20/system-polyfills.js"></script>
+  <script src="https://unpkg.com/angular2/es6/dev/src/testing/shims_for_IE.js"></script>
 @endsection
