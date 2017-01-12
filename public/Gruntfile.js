2f2809b4cb548872492c8abf2eea8a4f0515a5c8
Renuka Ghate <renuka@ajency.in>
2017-01-12 19:41:28 +0530

    Corrected the login and sign up pages not appearing in the center and showing a scroll bar, corrected some css issues and made the mobile navigation match the angular branch’s navigation style, added grunt to compile less

---
+++ public/Gruntfile.js
@@ -0,0 +1,31 @@
+module.exports = function(grunt) {
+  require('jit-grunt')(grunt);
+
+  grunt.initConfig({
+    less: {
+      development: {
+        options: {
+          compress: false,
+          yuicompress: false,
+          optimization: 2
+        },
+        files: {
+          "css/styles.css": "less/styles.less" // destination file and source file
+          // "../../../public/views/assets/css/styles.css": "less/styles.less",
+          // "../../../public/assets/css/styles.css": "less/styles.less"
+        }
+      }
+    },
+    watch: {
+      styles: {
+        files: ['less/**/*.less'], // which files to watch
+        tasks: ['less'],
+        options: {
+          nospawn: true
+        } 
+      }
+    }
+  });
+
+  grunt.registerTask('default', ['less', 'watch']);
+};
