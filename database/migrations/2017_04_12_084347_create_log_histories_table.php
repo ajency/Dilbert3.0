f2a6a0cfad999c0f68c92d605d5265b5f5366d44
SharathSkN <sharath@ajency.in>
2017-04-12 14:15:49 +0530

    Created new Table & Model LogHistory

---
+++ database/migrations/2017_04_12_084347_create_log_histories_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLogHistoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('log_histories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('user_id');
+            $table->unsignedInteger('modified_by');
+            $table->date('modified_on');
+            $table->string('table_modified');
+            $table->string('column_modified');
+            $table->string('old_value');
+            $table->string('new_value');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('log_histories');
+    }
+}
