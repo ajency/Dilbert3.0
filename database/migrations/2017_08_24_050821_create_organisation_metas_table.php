fc2545e2eb4b1a88d5daca502335831a7fb559e7
Sharang Sinai Amonkar <sharang@ajency.in>
2017-08-24 10:43:24 +0530

    added oragnisation meta migration

---
+++ database/migrations/2017_08_24_050821_create_organisation_metas_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrganisationMetasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('organisation_metas', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('organisation_id');
+            $table->string('key');
+            $table->string('value');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('organisation_metas');
+    }
+}
